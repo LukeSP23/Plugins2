7c45698c5f7e70bb200c3a39b0cfeb9f9aba00b8
LukeSP23 <lukesp.2003@gmail.com>
2025-05-06 21:54:59 -0400

    3 Types of Notifications

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {
   LocalNotifications,
   PermissionStatus,
+  ScheduleEvery,
   ScheduleOptions,
 } from '@capacitor/local-notifications';
 
@@ -29,21 +30,67 @@ export class Tab1Page {
     }
   }
 
-  async scheduleNotification(seconds: number) {
+  async timeNotification(seconds: number) {
     try {
       await LocalNotifications.schedule({
         notifications: [
           {
             id: 1,
-            title: 'New Notification',
-            body: 'This is a test',
+            title: 'Timed Notification',
+            body: 'This notification is scheduled to appear after a delay of 10 seconds.',
             schedule: {
               at: new Date(
                 // get current time
                 new Date().getTime() +
-                  // add 3 seconds
                   seconds * 1000
               ),
+              every: 'second',
+              count: 5,
+              on: {
+                second: 50}
+            },
+          },
+        ],
+      });
+    } catch (error) {
+      console.error('Error scheduling notification:', error);
+    }
+  }
+
+  async scheduleNotification(hour: number, minute: number) {
+    try {
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            id: 2,
+            title: 'Scheduled Notification',
+            body: 'This notification is scheduled to appears at 8AM daily.',
+            schedule: {
+              at: new Date(
+                new Date().setHours(hour, minute, 0, 0)
+              ),
+              every: 'day',
+            },
+          },
+        ],
+      });
+    } catch (error) {
+      console.error('Error scheduling notification:', error);
+    }
+  }
+
+  async repeatNotification(repeatArg?: boolean) {
+    try {
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            id: 3,
+            title: 'Repeat Notification',
+            body: 'This notification is scheduled to repeat 5 times every 50 seconds.',
+            schedule: {
+              at: new Date(Date.now() + 50 * 1000),
+              every: 'minute',
+              repeats: repeatArg,
             },
           },
         ],
